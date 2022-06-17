57bc67940e7b929a7e17050b382831889506971c
pojo <pojo@163.com>
2022-06-17 15:01:15 +0800

    remote push test

--- src/git_demo/GitTest.java
+++ src/git_demo/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("master branch conflicts modified");
 
         System.out.println("hot-fix branch conflicts modified");
+
+        System.out.println("remote push test");
     }
 }
